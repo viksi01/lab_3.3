86b9f09ad2510af67405ee48c241f62152881b00
viksi01 <victoria.pyatnicka@gmail.com>
2022-10-14 15:22:55 +0300

    commit1_master

--- lab_3.3/lab_3.3.cpp
+++ lab_3.3/lab_3.3.cpp
@@ -14,18 +14,19 @@ int main()
 	cout << "x = "; cin >> x;
 
 	if (x <= -1)
-		y = x + 1;
+		y = -x - 1;
 	else
 		if (-1 < x && x <= 1)
 			y = 0;
 		else
 			if (1 < x && x <= 1 + 2*R)
-				y = sqrt ((R * R) - (x - 1 + R)* (x - 1 + R));
+				y = sqrt ((R * R) - (x - 1 - R)* (x - 1 - R));
 			else
 				y= - (x - 1 - 2 * R) / (6 - 2 * R);
 
 	cout << endl;
 	cout << "y = " << y << endl;
+
 	cin.get();
 	return 0;
 }
